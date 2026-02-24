7b9cdaeb83fb7a1deeb7e00010e665e2f024ec6d
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2026-02-24 15:27:18 -0600

    owning bus variants

--- src/virtual/protocols/WithShaderProtocol.h
+++ src/virtual/protocols/WithShaderProtocol.h
@@ -17,89 +17,84 @@
 namespace npb
 {
 
-template<typename TColor, typename TSettings>
-struct WithShaderProtocolSettings : public TSettings
-{
-    ResourceHandle<IShader<TColor>> shader;
-};
-
-template<typename TShader, typename TSettings>
-struct WithOwnedShaderProtocolSettings : public TSettings
-{
-    TShader shader;
-};
-
-template<typename TColor = Color, typename TProtocol = IProtocol<TColor>>
-    requires std::derived_from<TProtocol, IProtocol<TColor>>
-class WithShader : public TProtocol
-{
-public:
-    using SettingsType = WithShaderProtocolSettings<TColor, typename TProtocol::SettingsType>;
-
-    template<typename... TArgs>
-    WithShader(uint16_t pixelCount,
-               SettingsType settings,
-               TArgs&&... args)
-        : TProtocol(pixelCount, std::forward<TArgs>(args)...)
-        , _shader{std::move(settings.shader)}
-        , _scratchColors(pixelCount)
+    template <typename TColor, typename TSettings>
+    struct WithShaderProtocolSettings : public TSettings
     {
-    }
+        ResourceHandle<IShader<TColor>> shader;
+    };
 
-    void update(std::span<const TColor> colors) override
+    template <typename TShader, typename TSettings>
+    struct WithOwnedShaderProtocolSettings : public TSettings
     {
-        std::span<const TColor> source = colors;
-        if (nullptr != _shader)
+        TShader shader;
+    };
+
+    template <typename TColor = Color, typename TProtocol = IProtocol<TColor>>
+        requires std::derived_from<TProtocol, IProtocol<TColor>>
+    class WithShader : public TProtocol
+    {
+    public:
+        using SettingsType = WithShaderProtocolSettings<TColor, typename TProtocol::SettingsType>;
+
+        template <typename... TArgs>
+        WithShader(uint16_t pixelCount,
+                   SettingsType settings,
+                   TArgs &&...args)
+            : TProtocol(pixelCount, std::forward<TArgs>(args)...), _shader{std::move(settings.shader)}, _scratchColors(pixelCount)
         {
-            std::copy(colors.begin(), colors.end(), _scratchColors.begin());
-            _shader->apply(_scratchColors);
-            source = _scratchColors;
         }
 
-        TProtocol::update(source);
-    }
+        void update(std::span<const TColor> colors) override
+        {
+            std::span<const TColor> source = colors;
+            if (nullptr != _shader)
+            {
+                std::copy(colors.begin(), colors.end(), _scratchColors.begin());
+                _shader->apply(_scratchColors);
+                source = _scratchColors;
+            }
 
-private:
-    ResourceHandle<IShader<TColor>> _shader;
-    std::vector<TColor> _scratchColors;
-};
+            TProtocol::update(source);
+        }
 
-template<size_t TPixelCount,
-         typename TColor = Color,
-         typename TShader = IShader<TColor>,
-         typename TProtocol = IProtocol<TColor>>
-    requires (std::derived_from<TProtocol, IProtocol<TColor>> &&
-              std::derived_from<TShader, IShader<TColor>>)
-class WithOwnedShader : public TProtocol
-{
-public:
-    using SettingsType = WithOwnedShaderProtocolSettings<TShader, typename TProtocol::SettingsType>;
+    private:
+        ResourceHandle<IShader<TColor>> _shader;
+        std::vector<TColor> _scratchColors;
+    };
 
-    template<typename... TArgs>
-    WithOwnedShader(uint16_t pixelCount,
-                    SettingsType settings,
-                    TArgs&&... args)
-        : TProtocol(pixelCount, std::forward<TArgs>(args)...)
-        , _shader{std::move(settings.shader)}
+    template <size_t TPixelCount,
+              typename TColor = Color,
+              typename TShader = IShader<TColor>,
+              typename TProtocol = IProtocol<TColor>>
+        requires(std::derived_from<TProtocol, IProtocol<TColor>> &&
+                 std::derived_from<TShader, IShader<TColor>>)
+    class WithOwnedShader : public TProtocol
     {
-    }
+    public:
+        using SettingsType = WithOwnedShaderProtocolSettings<TShader, typename TProtocol::SettingsType>;
 
-    void update(std::span<const TColor> colors) override
-    {
-        const size_t colorCount = std::min(colors.size(), _scratchColors.size());
-        std::copy_n(colors.begin(), colorCount, _scratchColors.begin());
+        template <typename... TArgs>
+        WithOwnedShader(uint16_t pixelCount,
+                        SettingsType settings,
+                        TArgs &&...args)
+            : TProtocol(pixelCount, std::forward<TArgs>(args)...), _shader{std::move(settings.shader)}
+        {
+        }
 
-        std::span<TColor> shadedColors{_scratchColors.data(), colorCount};
-        _shader.apply(shadedColors);
+        void update(std::span<const TColor> colors) override
+        {
+            const size_t colorCount = std::min(colors.size(), _scratchColors.size());
+            std::copy_n(colors.begin(), colorCount, _scratchColors.begin());
 
-        TProtocol::update(std::span<const TColor>{_scratchColors.data(), colorCount});
-    }
+            std::span<TColor> shadedColors{_scratchColors.data(), colorCount};
+            _shader.apply(shadedColors);
 
-private:
-    TShader _shader;
-    std::array<TColor, TPixelCount> _scratchColors{};
-};
+            TProtocol::update(std::span<const TColor>{_scratchColors.data(), colorCount});
+        }
 
-
+    private:
+        TShader _shader;
+        std::array<TColor, TPixelCount> _scratchColors{};
+    };
 
 } // namespace npb
