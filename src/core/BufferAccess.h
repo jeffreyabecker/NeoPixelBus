9ee1782d140dca77cb4b87bf3b1cd5ff6e257c28
Jeffrey Becker <jeffrey.a.becker@gmail.com>
2026-03-02 16:56:21 -0600

    core: add buffer access seam interfaces

---
+++ src/core/BufferAccess.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+#include "core/Compat.h"
+
+namespace lw
+{
+
+    struct ProtocolSliceRange
+    {
+        size_t offset{0};
+        size_t size{0};
+    };
+
+    template <typename TColor>
+    class IBufferAccess
+    {
+    public:
+        virtual ~IBufferAccess() = default;
+
+        virtual span<TColor> rootPixels() = 0;
+        virtual span<const TColor> rootPixels() const = 0;
+
+        virtual span<TColor> shaderScratch() = 0;
+        virtual span<const TColor> shaderScratch() const = 0;
+
+        virtual span<uint8_t> protocolArena() = 0;
+        virtual span<const uint8_t> protocolArena() const = 0;
+
+        virtual size_t protocolSliceCount() const = 0;
+        virtual span<uint8_t> protocolSlice(size_t strandIndex) = 0;
+        virtual span<const uint8_t> protocolSlice(size_t strandIndex) const = 0;
+    };
+
+    template <typename TColor>
+    class IBufferAccessProvider
+    {
+    public:
+        virtual ~IBufferAccessProvider() = default;
+
+        virtual IBufferAccess<TColor> &bufferAccess() = 0;
+        virtual const IBufferAccess<TColor> &bufferAccess() const = 0;
+    };
+
+} // namespace lw
